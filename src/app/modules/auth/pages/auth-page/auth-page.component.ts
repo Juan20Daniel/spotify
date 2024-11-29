ab44a261317f68fc3cd082d459f1ce86b5461e38
Juan20Daniel <juandaniel200031@gmail.com>
2024-11-29 12:16:26 -0600

    Maquetación completa del login

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,15 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.css'
 })
 export class AuthPageComponent {
-
+  formLogin: FormGroup = new FormGroup({});
+  ngOnInit():void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('',[
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl('',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12)
+      ])
+    })
+  }
+  login():void {
+    if(this.formLogin.invalid) return;
+    const body = this.formLogin.value;
+    console.log(body);
+  }
 }
